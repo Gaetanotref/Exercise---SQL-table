a1be79ebe37d8670a2aeac2677305c299755675f
Gaetano Trefiletti <gaetano.trefiletti.gt@gmail.com>
2023-04-06 15:31:44 +0200

    Exercise sql 2

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/newdb", "developer", "developer");
+            Statement statement = connection.createStatement();
+            statement.execute("INSERT INTO test1 (name,surname)values('Franco','Rossi')");
+            ResultSet resultSet = statement.executeQuery("select * from test1");
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
